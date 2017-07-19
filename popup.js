b7bece83b7199d51c28db768effa84b3cb0cbc60
Tappin <tappina@americas.corpdir.net>
2017-07-19 16:51:17 -0700

    fake data hooked up

--- popup.js
+++ popup.js
@@ -5,4 +5,21 @@ var app = angular.module('app', []);
 app.controller('appCtrl', function($scope, $q, $location) {
   $scope.hello = "hello world";
   $scope.selected = 'weekly';
+  $scope.projects = [
+    {
+      name: 'WSNLA',
+      weekly: '28h 10m 39s',
+      total: '93h 10m 39s'
+    },
+    {
+      name: 'DTNA',
+      weekly: '18h 10m 39s',
+      total: '193h 10m 39s'
+    },
+    {
+      name: 'Nike',
+      weekly: '10h 12m 36s',
+      total: '13h 10m 31s'
+    }
+  ]
 });
